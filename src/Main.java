a62c7fc2af94c93114539c5b71aa12721352d111
LeonelLexa <nartakhov@icloud.com>
2024-05-30 13:29:01 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Акция 100 + 1");
+
+        int balance = 500; // Баланс до пополнения
+
+        System.out.println("Ваш баланс = " + balance);
+
+        int replenishment = 1200; // Сумма пополнения
+
+        System.out.println("Сумма пополнения = " + replenishment);
+
+        int bonus = 0;
+
+        if (replenishment > 1000) {
+            bonus = (replenishment) / 100;
+        }
+
+        int totalBalance = balance + replenishment + bonus;
+
+        System.out.println("На вашем счету = " + totalBalance);
+        System.out.println("Бонусных рублей = " + bonus);
+
     }
 }
